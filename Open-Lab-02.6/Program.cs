1583854fd782b4d44235b136136b5badadbcb840
z18ista03 <z18ista03@Z00P1-10.SPSKNM>
2019-10-14 09:32:34 +0200

    Add project files.

---
+++ Open-Lab-02.6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Open_Lab_02._6
+{
+    class Program
+    {
+        public static bool Divisible(int num)
+        {
+            return (num % 100 == 0);
+        }
+        static void Main(string[] args)
+        {
+            bool divisible;
+            int num;
+
+            Console.WriteLine("Zadaj si číslo:");
+            num = int.Parse(Console.ReadLine());
+
+            divisible = Divisible(num);
+            Console.WriteLine(divisible);
+        }
+    }
+}
